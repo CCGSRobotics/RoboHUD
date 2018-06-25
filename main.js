83de9cdf5a5dc2f285ca8256756146405839cb52
Angus Finch <developer.finchie@gmail.com>
2018-06-25 20:21:34 +0800

    Initial Commit

---
+++ main.js
@@ -0,0 +1,25 @@
+const {app, BrowserWindow} = require('electron')
+let win
+
+function createWindow () {
+  win = new BrowserWindow({width: 800, height: 600})
+  win.loadFile('HTML/version.html')
+
+  win.on('closed', () => {
+    win = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow()
+  }
+})
