9787030d8dea908c5f72568e96ff5d0ae3d49c85
Angus Finch <developer.finchie@gmail.com>
2018-06-30 19:53:32 +0800

    Add sidenav & restructure layout

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ function createWindow() {
     width: 800,
     height: 600
   })
-  win.loadFile('App/HTML/index.html')
+  win.loadFile('App/index.html')
 
   win.on('closed', () => {
     win = null
