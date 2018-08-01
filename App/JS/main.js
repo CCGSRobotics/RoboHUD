81a596138720ab134beb23b1fce91a23e1be4ae3
Angus Finch <developer.finchie@gmail.com>
2018-08-01 19:51:37 +0800

    Add Python spawn test

--- App/JS/main.js
+++ App/JS/main.js
@@ -1,2 +1,8 @@
 var shell = require('shelljs');
-var util = require('util');
+const spawn = require('child_process').spawn;
+
+const runTests = spawn('python', ['Resources/Python/test.py', 42]);
+
+runTests.stdout.on('data', (data) => {
+  console.log(data)
+})
