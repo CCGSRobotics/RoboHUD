989fd2d99580968827722aa3dfd5fb6d55021709
NickTheGreek1985 <1019912@student.ccgs.wa.edu.au>
2018-09-11 17:48:56 +0800

    Add working controller support & communication with server

--- App/JS/main.js
+++ App/JS/main.js
@@ -1,17 +1,1 @@
-var shell = require('shelljs');
 
-var PythonShell = require('python-shell');
-var pyshell = new PythonShell('App/JS/Resources/Python/test.py');
-
-pyshell.send('hello');
-
-pyshell.on('message', function (message) {
-  console.log('Message from Python: ' + message);
-});
-
-pyshell.end(function (err,code,signal) {
-  if (err) throw err;
-  console.log('Execution finished.')
-  console.log('The exit code was: ' + code);
-  console.log('The exit signal was: ' + signal);
-});
