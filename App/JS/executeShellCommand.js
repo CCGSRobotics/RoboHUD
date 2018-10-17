6cea7f52895f0b885516f83a7b66abb921b26bae
finchiedev <developer.finchie@gmail.com>
2018-10-17 16:58:06 +0800

    Add autostart buttons

--- App/JS/executeShellCommand.js
+++ App/JS/executeShellCommand.js
@@ -1,4 +1,5 @@
 var exec = require('child_process').exec;
+var hasErrored = 0
 
 function execute(command) {
 	command = String(command);
@@ -7,8 +8,13 @@ function execute(command) {
 	    console.log('Stdout: ' + stdout);
 	    console.error('Stderr: ' + stderr);
 	    if (error !== null) {
-	         alert('Execution Error. Try looking at the documentation for possible sources of error. If the problem persists, please report this with the following error message:\n' + error)
-	    }
+			if (hasErrored < 5) {
+		         alert('Execution Error. Try looking at the documentation for possible sources of error. If the problem persists, please report this with the following error message:\n' + error)
+				 hasErrored += 1
+			} else {
+				console.warn("Execution Error, but max prompt limit reached. " + error)
+			}
+		}
 	});
 };
 
