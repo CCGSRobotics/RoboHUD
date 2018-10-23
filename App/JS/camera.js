f8b171a81c1a4dd6561e6446001ad905c4b19bee
finchiedev <developer.finchie@gmail.com>
2018-10-23 18:10:32 +0800

    Add camera command

--- App/JS/camera.js
+++ App/JS/camera.js
@@ -1,16 +1,24 @@
-const http               = require('http');
-const express            = require('express');
-const RemoteTCPFeedRelay = require('./lib/remotetcpfeed');
-const app                = express();
+var settings = require("./JS/Resources/settings.json")
+var width = settings.User.Camera["Width"]
+var height = settings.User.Camera["Height"]
+var time = settings.User.Camera["Time"]
+var extraCommands = settings.User.Camera["Extra Commands"]
+var port = settings.User.Camera.Network["Port"]
+var ip = settings.User.Camera.Network["IP"]
+var command = `raspivid -w ${width} -h ${height} -t ${time} ${extraCommands} | nc ${ip} ${port}`
 
-app.use(express.static(__dirname + '/public'));
-app.use(express.static(__dirname + '/vendor/dist'));
-
-const server  = http.createServer(app);
-
-const feed    = new RemoteTCPFeedRelay(server, {
-  feed_ip   : "192.168.100.1",
-  feed_port : 8080,
-});
-
-server.listen(5001);
+//Put in default text
+function loaded() {
+    console.log("Loaded!")
+    document.forms[0].elements[0].value = command
+    document.forms[0].elements[2].placeholder = height
+    document.forms[0].elements[3].placeholder = width
+    document.forms[0].elements[4].placeholder = time
+    document.forms[0].elements[5].placeholder = port
+    document.getElementById('settingsIP').innerHTMl = ip
+    console.log(document.getElementById('settingsIP'))
+    console.log(document.forms)
+}
+function startCamera() {
+    console.log("placeholder")
+}
