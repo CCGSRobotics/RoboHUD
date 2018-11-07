396f5b2974f9595f7ddccf84f89e7ccc458c2fdf
finchiedev <developer.finchie@gmail.com>
2018-11-07 16:06:16 +0800

    Add chart.js compatibility

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ function createWindow() {
     width: 800,
     height: 600
   })
-  win.loadFile('App/control.html')
+  win.loadFile('App/charts.html')
   // win.loadURL('http://localhost:8080')
   win.webContents.openDevTools()
 
