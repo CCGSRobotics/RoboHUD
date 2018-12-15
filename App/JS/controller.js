e71b17d9d9c2d8ddc7ec629c6b950be245498282
finchiedev <developer.finchie@gmail.com>
2018-12-15 10:48:19 +0800

    Add number of axis/button in controller view

--- App/JS/controller.js
+++ App/JS/controller.js
@@ -21,6 +21,11 @@ setInterval(function(){
       var gamepad = gamepads[i];
 
       if (gamepad !== null && i !== 4) {
+        var info = document.createElement('h3');
+        info.setAttribute('id', `ID ${i}`);
+        info.innerHTML = String(gamepad.id);
+        parent.appendChild(info)
+
         var table = document.createElement('table');
         table.setAttribute('id', `Controller ${i}`)
         parent.appendChild(table)
@@ -29,28 +34,33 @@ setInterval(function(){
         infoRow.setAttribute('id', 'info');
         table.appendChild(infoRow);
 
-        for (var c = 0; c < 2; c++) {
+        for (var c = 0; c <= 2; c++) {
           var heading = document.createElement('th');
           heading.setAttribute('id', `Info ${c}`);
           infoRow.appendChild(heading);
         }
 
         document.getElementById('Info 0').innerHTML = 'Type';
-        document.getElementById('Info 1').innerHTML = 'Value';
+        document.getElementById('Info 1').innerHTML = 'Number';
+        document.getElementById('Info 2').innerHTML = 'Value';
 
         for (var x = 0; x < gamepad.axes.length; x++) {
           var row = document.createElement('tr');
           row.setAttribute('id', `Axis ${x}`)
           table.appendChild(row)
 
-          for (var z = 0; z <2; z++) {
+          for (var z = 0; z < 3; z++) {
             var data = document.createElement('td');
-            if (z == 1) {
+            if (z == 2) {
               data.setAttribute('id', `0, ${i}, ${x}`)
-            } else {
+            } else if (z == 0){
               data.setAttribute('id', `0, ${i}, ${x}, ${z}`);
               data.innerHTML = 'Axis';
               data.setAttribute('class', 'axis');
+            } else {
+              data.setAttribute('id', `0, ${i}, ${x}, ${z}`);
+              data.innerHTML = `${x+1}`;
+              data.setAttribute('class', 'number');
             }
             table.appendChild(data);
           }
@@ -61,18 +71,44 @@ setInterval(function(){
           row.setAttribute('id', `Axis ${y}`)
           table.appendChild(row)
 
-          for (var z = 0; z < 2; z++) {
+          for (var z = 0; z < 3; z++) {
             var data = document.createElement('td');
-            if (z == 1) {
+            if (z == 2) {
               data.setAttribute('id', `1, ${i}, ${y}`)
-            } else {
+            } else if (z == 0){
               data.setAttribute('id', `1, ${i}, ${y}, ${z}`);
               data.innerHTML = 'Button';
               data.setAttribute('class', 'button');
+            } else {
+              data.setAttribute('id', `1, ${i}, ${y}, ${z}`);
+              data.innerHTML = `${y+1}`;
+              data.setAttribute('class', 'number');
             }
             table.appendChild(data);
           }
         }
+
+        // for (var y = 0; y < gamepad.buttons.length; y++) {
+        //   var row = document.createElement('tr');
+        //   row.setAttribute('id', `Axis ${y}`)
+        //   table.appendChild(row)
+        //
+        //   for (var z = 0; z < 2; z++) {
+        //     var data = document.createElement('td');
+        //     if (z == 2) {
+        //       data.setAttribute('id', `1, ${i}, ${y}`)
+        //     } else if (z == 0){
+        //       data.setAttribute('id', `1, ${i}, ${y}`);
+        //       data.innerHTML = 'Button';
+        //       data.setAttribute('class', 'button');
+        //     } else {
+        //       data.setAttribute('id', `1, ${i}, ${y}`);
+        //       data.innerHTML = `${y+1}`;
+        //       data.setAttribute('class', 'number');
+        //     }
+        //     table.appendChild(data);
+        //   }
+        // }
 
         // for (var y = 0; y < gamepad.buttons.length; y++) {
         //   var button = document.createElement('p');
