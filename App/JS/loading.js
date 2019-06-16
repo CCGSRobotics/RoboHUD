e5df3e5f010fa2c9ede0c146f4cc13205b749d75
Angus Finch <developer.finchie@gmail.com>
2019-06-16 21:07:52 +0800

    Migrate loading scripts to loading.js

--- App/JS/loading.js
+++ App/JS/loading.js
@@ -1,3 +1,5 @@
+// Place a random sentence in the loading animation
+
 var sentences = require('./JS/Resources/sentences.json');
 
 for (var i = 0; i < sentences.length; i++) {
@@ -5,3 +7,17 @@ for (var i = 0; i < sentences.length; i++) {
 }
 
 document.getElementById("loading-text").innerHTML = `${sentences[Math.round(Math.random() * (sentences.length - 1))]}...`;
+
+// Tell the server to start
+
+client.connect(5000, '192.168.100.1', function() {
+  console.log("Connected to server interface");
+})
+
+function writeToServer(data) {
+  client.write(data);
+  console.log(`Wrote '${data}' to server`);
+}
+
+writeToServer('VIDEO');
+writeToServer('START');
