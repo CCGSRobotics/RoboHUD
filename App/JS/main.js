8d39917d660016be4217acfe7fc7f60664dcdc63
Angus Finch <developer.finchie@gmail.com>
2019-06-16 21:09:02 +0800

    Migrate settings to main.js

--- App/JS/main.js
+++ App/JS/main.js
@@ -1,16 +1,18 @@
-const execute = require('./JS/executeShellCommand.js').execute;
-
-function controlServerCode(terminate) {
-    if (terminate == true) {
-        console.log(terminate)
-        execute("sshpass -p \"raspberry\" ssh pi@192.168.100.1 'pkill -f \"python3 Desktop/Server-Code-2018/RoboCupServer-Current.py\"'");
-    } else {
-        execute("sshpass -p \"raspberry\" ssh pi@192.168.100.1 'python3 Desktop/Server-Code-2018/RoboCupServer-Current.py'");
-        console.log("Server initiation command sent")
-        document.getElementById('status').innerHTML = "Starting..."
-        setTimeout(function() {
-            execute('python3 App/JS/Resources/CurrentEmuBotCode2/client.py');
-            document.getElementById('status').innerHTML = "Vroom Vroom!"
-        }, 3000)
-    }
-}
+var net = require('net');
+var dgram = require('dgram');
+var client = new net.Socket();
+var childProcess = require('child_process');
+var clientSocket = new dgram.createSocket('udp4');
+const settings = require('./JS/Resources/Settings/flipperBot.json')
+const jointSpeed = settings["Client"]["Controller"]["Joints"].JointSpeed
+const panDirectionDefault = settings["Client"]["Controller"]["Joints"].PanDirection
+const tiltDirectionDefault = settings["Client"]["Controller"]["Joints"].TiltDirection
+const leftCameraButton = settings["Client"]["Controller"]["Buttons"].CameraPanLeft
+const rightCameraButton = settings["Client"]["Controller"]["Buttons"].CameraPanRight
+const upCameraButton = settings["Client"]["Controller"]["Buttons"].CameraTiltUp
+const downCameraButton = settings["Client"]["Controller"]["Buttons"].CameraTiltDown
+var panDirection = panDirectionDefault
+var tiltDirection = tiltDirectionDefault
+var video;
+var lastVals = [];
+var multipliers = [[false, -1], [false, -1]]
