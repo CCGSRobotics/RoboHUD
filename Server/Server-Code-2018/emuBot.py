932de9a0197716c764db96b53ab5eec159e89902
Ben Lane <ben.lane65@gmail.com>
2019-06-20 15:04:10 +0800

    Update emuBot.py

--- Server/Server-Code-2018/emuBot.py
+++ Server/Server-Code-2018/emuBot.py
@@ -28,39 +28,39 @@ def jointMode(ID):
   if ID < 9:
     dxl_comm_result, dxl_error = packetHandler.write4ByteTxRx(portHandler, ID, 6, 1024)
     if dxl_comm_result != COMM_SUCCESS:
-        print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
+      print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
     elif dxl_error != 0:
-        print("%s" % packetHandler.getRxPacketError(dxl_error))
+      print("%s" % packetHandler.getRxPacketError(dxl_error))
 
-     dxl_comm_result, dxl_error = packetHandler.write4ByteTxRx(portHandler, ID, 8, 3072)
+    dxl_comm_result, dxl_error = packetHandler.write4ByteTxRx(portHandler, ID, 8, 3072)
     if dxl_comm_result != COMM_SUCCESS:
-        print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
+      print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
     elif dxl_error != 0:
-        print("%s" % packetHandler.getRxPacketError(dxl_error))
+      print("%s" % packetHandler.getRxPacketError(dxl_error))
         
   elif ID == 9:    
     dxl_comm_result, dxl_error = packetHandler.write4ByteTxRx(portHandler, ID, 6, 0)
     if dxl_comm_result != COMM_SUCCESS:
-        print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
+      print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
     elif dxl_error != 0:
-        print("%s" % packetHandler.getRxPacketError(dxl_error))
+      print("%s" % packetHandler.getRxPacketError(dxl_error))
 
     dxl_comm_result, dxl_error = packetHandler.write4ByteTxRx(portHandler, ID, 8, 2560)
     if dxl_comm_result != COMM_SUCCESS:
-        print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
+      print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
     elif dxl_error != 0:
       print("%s" % packetHandler.getRxPacketError(dxl_error))
       
   elif ID > 9:
     dxl_comm_result, dxl_error = packetHandler.write4ByteTxRx(portHandler, ID, 6, 0)
     if dxl_comm_result != COMM_SUCCESS:
-        print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
+      print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
     elif dxl_error != 0:
-        print("%s" % packetHandler.getRxPacketError(dxl_error))
+      print("%s" % packetHandler.getRxPacketError(dxl_error))
 
     dxl_comm_result, dxl_error = packetHandler.write4ByteTxRx(portHandler, ID, 8, 1023)
     if dxl_comm_result != COMM_SUCCESS:
-        print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
+      print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
     elif dxl_error != 0:
       print("%s" % packetHandler.getRxPacketError(dxl_error))
 
@@ -69,51 +69,49 @@ def jointMode(ID):
 def wheelMode(ID):
   dxl_comm_result, dxl_error = packetHandler.write4ByteTxRx(portHandler, ID, 6, 0)
   if dxl_comm_result != COMM_SUCCESS:
-      print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
+    print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
   elif dxl_error != 0:
-      print("%s" % packetHandler.getRxPacketError(dxl_error))
+    print("%s" % packetHandler.getRxPacketError(dxl_error))
 
-   dxl_comm_result, dxl_error = packetHandler.write4ByteTxRx(portHandler, ID, 8, 0)
+  dxl_comm_result, dxl_error = packetHandler.write4ByteTxRx(portHandler, ID, 8, 0)
   if dxl_comm_result != COMM_SUCCESS:
-      print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
+    print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
   elif dxl_error != 0:
-      print("%s" % packetHandler.getRxPacketError(dxl_error))
+    print("%s" % packetHandler.getRxPacketError(dxl_error))
 
-# Move a dynamixel that has been set up as a joint
-def moveJoint(ID, position, speed):
-  # print(ID,position,speed)
-  sc.goto(int(ID), int(position), int(speed), False)
-
-# Move a dynamixel that has been set up as a wheel
-# Negative speed moves CW, positive speed moves CCW
-
-def moveJointM(ID, position):
+def moveJoint(ID, position,speed):
   dxl_comm_result, dxl_error = packetHandler.write4ByteTxRx(portHandler, ID, 30, position)
   if dxl_comm_result != COMM_SUCCESS:
-      print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
+    print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
   elif dxl_error != 0:
-      print("%s" % packetHandler.getRxPacketError(dxl_error))
-  
+    print("%s" % packetHandler.getRxPacketError(dxl_error))
+ 
   dxl_present_position, dxl_comm_result, dxl_error = packetHandler.read4ByteTxRx(portHandler, ID, 36)
   if not abs(dxl_goal_position[index] - dxl_present_position) > 20: 
     print("Servo with ID of %03d was %03d off target, outside acceptable range" % (ID, abs(position - dxl_present_position)))
 
+  dxl_comm_result, dxl_error = packetHandler.write4ByteTxRx(portHandler, ID, 32, speed)
+  if xl_comm_result != COMM_SUCCESS:
+    print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
+  elif dxl_error != 0:
+    print("%s" % packetHandler.getRxPacketError(dxl_error))
+    
 def moveWheel(ID, speed):
   # Convert negative values of speed to between 1024 and 2047
   if speed < 0:
-      # Limit allowed reverse speed to prevent errors
-      if speed < -1024:
-          speed = 2047
-      else:
-          speed = 1023 + -speed
+     # Limit allowed reverse speed to prevent errors
+    if speed < -1024:
+      speed = 2047
+    else:
+      speed = 1023 + -speed
 
   else:
-      if speed > 1023:
-          # Limit allowed forward speed to prevent errors
-          speed = 1023
+    if speed > 1023:
+       # Limit allowed forward speed to prevent errors
+      speed = 1023
 
   dxl_comm_result, dxl_error = packetHandler.write4ByteTxRx(portHandler, ID, 32, speed)
-  if dxl_comm_result != COMM_SUCCESS:
-      print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
+  if xl_comm_result != COMM_SUCCESS:
+    print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
   elif dxl_error != 0:
-      print("%s" % packetHandler.getRxPacketError(dxl_error))
+    print("%s" % packetHandler.getRxPacketError(dxl_error))
