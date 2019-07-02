30c0cea28e7e9cb2e6f2f101c538e6197c9a884d
toddeee <50311910+toddeee@users.noreply.github.com>
2019-07-02 10:26:49 +0800

    Made to work only with the Grabber

--- Server/Server-Sensor-Code/Grabber.py
+++ Server/Server-Sensor-Code/Grabber.py
@@ -6,7 +6,6 @@ arduino_port_address = "COM8" # Use "COM[NUMBER]"" for Windows | "/dev/ttyUSB0" 
 
 serialLine = serial.Serial(arduino_port_address, baudrate=115200, timeout=5)
 
-numOfSensors = 2
 grabberServos = [1,2,3]
 cameraServos = [4,5]
 
@@ -16,36 +15,8 @@ def moveMicroServo(ID, Value):
     out =str(ID) + "s" + str(Value)
     serialLine.write(bytearray(out, 'utf8'))
 
-def readData():
-    # The entire line of serial communication is read and cleaned
-    line = str(serialLine.readline())
-    #line = line.replace("b","")
-    #line = line.replace("\\","")
-    line = line[2:-5]
-    
-    
-    
-    ''' sensor_identifier is the identifer of the sensor, set in the Ardiuno's memory.
-        sensor_value is the analog value read from the sensor. '''
-    
-    if line[0].isalpha() and line[1:].isdigit():
-        # "sensor" is a dictionary that has two attributes, "value" and "identifier"
-        sensor = {}
-        sensor['identifier'] = line[0]
-        sensor['value'] = int(line[1:])
-        return sensor
 
-    return {'identifier': 'error', 'value': 0}
     
-    ''' This loop goes though each character of "string"
-        and retrives the analog value from the string '''
-    '''for x in range(1,len(string)):
-        if string[x] != '\\':
-            sensor_value = sensor_value + string[x]
-        else:
-            break'''
-        
-
 def moveGrabber(percentage):
     moveMicroServo(1, percentage+10)
     time.sleep(0.1)
@@ -57,7 +28,10 @@ def moveCameras(positions):
     moveMicroServo(4, int(positions[0]))
     time.sleep(0.1)
     moveMicroServo(5, int(positions[1])+10)
+    
 
+
+    
 print("Arduino Communication Line established. All systems are go!")
 #CODE BELOW IS TO RUN THIS CODE ON THE SERVER
 class UDPServerHandler(SocketServer.BaseRequestHandler):
@@ -65,25 +39,22 @@ class UDPServerHandler(SocketServer.BaseRequestHandler):
         self.data = self.request[0].decode('utf-8').strip()
         if "," in self.data:
             moveCameras(self.data.split(","))
-        elif "sense" in self.data:
-            socket = self.request[1]
-            sensor = readData()
-            socket.sendto(bytes(sensor['identifier'] + str(sensor['value']) + "\n", "utf-8"), self.client_address)
         else:
             moveGrabber(int(self.data))
-        
+            
+            
+#if __name__ == "main":
+HOST, PORT = "127.0.0.1", 25565
+SocketServer.UDPServer.allow_reuse_address = True
 
 
 
-if __name__ == "main":
-    HOST, PORT = "", 25565
-    SocketServer.UDPServer.allow_reuse_address = True
-
-    print("Server started!")
-    server = SocketServer.UDPServer((HOST, PORT), UDPServerHandler)
-    server = server.serve_forever()
+print("Server started!")
+server = SocketServer.UDPServer((HOST, PORT), UDPServerHandler)
+server = server.serve_forever()
 
 ####
+
 sensor = ''
 '''
 while True:
@@ -98,10 +69,18 @@ while True:
     
         moveGrabber(servo_percentage_position)
     
+
     sensor = readData() # sensor = {'identifier': a Character, 'value': an Integer}
     print(sensor)
 
     # Convert to degrees C?
     if sensor['identifier'] == 't':
         sensor['value'] = round((57*(sensor['value']-20))/100,1)
-'''   
+'''
+ 
+
+
+
+
+
+  
