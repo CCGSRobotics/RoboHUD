f3ffadeb537fb2271f11ba618dab47266924cc47
finchiedev <developer.finchie@gmail.com>
2019-08-01 22:27:10 +0800

    Add basic manual controls

--- Server/Driving/server.py
+++ Server/Driving/server.py
@@ -1,20 +1,43 @@
 import socketserver
+import re
 from dynamixels import *
 
-wheels = {}
+servos = {}
 
-wheels[5] = initialise_dynamixel('ax-18a', 5, 1)
+# wheels[5] = initialise_dynamixel('ax-18a', 5, 1)
 
 class UDPHandler(socketserver.BaseRequestHandler):
   def handle(self):
     self.data = self.request[0].decode('utf-8').strip()
     new = self.data.split('\n')
     for i in new:
-      if i.count('-') == 1:
+      if len(re.findall("\((init|delete|modify|mode)\)-.+", i)) == 1:
+        # Fancy regular expression to check if server is sending other commands
+        # WARNING: This code is VERY experimental and will crash if
+        # The wrong input is given
+
+        cmd = re.match("\((init|delete|modify|mode)\)", i).group(1)
+        if cmd == "init":
+          ID, protocol = map(int, re.findall("-.+-.$", i)[0].split('-')[1:])
+          servos[ID] = initialise_dynamixel('ax-18a', ID, protocol)
+        elif cmd == "mode":
+          ID, mode = map(str, re.findall("-.+-.+$", i)[0].split('-')[1:])
+          ID = int(ID)
+          if ID in servos:
+            if mode == 'wheel':
+              print('wheel')
+              servos[ID].wheel_mode()
+            elif mode == 'joint':
+              servos[ID].joint_mode()
+        elif cmd == 'delete':
+          ID = int(re.findall('-.+$', i)[0][1:])
+          if ID in servos:
+            servos.pop(ID)
+      elif i.count('-') == 1:
         # Wheel
         ID, speed = map(int, i.split('-'))
-        if ID in wheels:
-          wheels[ID].write_value('Moving Speed', speed)
+        if ID in servos:
+          servos[ID].write_value('Moving Speed', speed)
       elif i.count('-') == 2:
         # Joint
         ID, pos, speed = map(int, i.split('-'))
