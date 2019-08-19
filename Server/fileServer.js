94979a34a467dbfd72443f3659084c095c02d651
finchiedev <developer.finchie@gmail.com>
2019-08-19 21:16:39 +0800

    Implement remote control table download

---
+++ Server/fileServer.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const net = require('net');
+let server = net.createServer()
+
+server.on('connection', function(socket) {
+  socket.on('data', function(data) {
+    let fileData = data.toString().split('\n');
+    const filename = fileData[0].replace('-', '');
+    fileData.shift();
+    fileData = fileData.join('\n');
+
+    fs.writeFile(`./Driving/Servos/${filename}.csv`, fileData, function(err) {
+      if (err) {
+        console.error('Failed to write file! Do you have access?');
+      }
+      console.log(`Saved the file as ${filename}.csv`);
+    });
+  })
+})
+
+server.listen(5004, '');
