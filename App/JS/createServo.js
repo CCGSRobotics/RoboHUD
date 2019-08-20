002dcce30f3a2a0979f9734b748fdf98f4839ab0
finchiedev <developer.finchie@gmail.com>
2019-08-20 20:44:59 +0800

    Implement download/upload CSV buttons

--- App/JS/createServo.js
+++ App/JS/createServo.js
@@ -2,7 +2,15 @@ const rp = require('request-promise');
 const cheerio = require('cheerio');
 const cheerioTableparser = require('cheerio-tableparser');
 const net = require('net');
+let link = '';
+let fileData = '';
+let filename = '';
+
 const client = new net.Socket();
+client.on('error', function(err) {
+  console.error('Unable to connect to the fileserver!' + '\n' + 
+  'Ensure that the robot is powered and active.');
+})
 
 /**
  * Parses the header of a given table
@@ -51,52 +59,46 @@ function generateCSV(html, indexes) {
     'Ensure that the argument "indexes" is valid!');
     return '';
   }
-
+  
   let fileData = '';
   const $ = cheerio.load(html);
   cheerioTableparser($);
   const cheerioTable = $('table');
-
+  
   fileData += parseDynamixelHeader(cheerioTable.eq(
     indexes[0]).parsetable(true, true, true));
   for (var i = 0; i < indexes.length; i++) {
     fileData += parseDynamixelTable(cheerioTable.eq(
       indexes[i]).parsetable(true, true, true));
+    }
+    
+    return fileData;
   }
-
-  return fileData;
-}
-
-function getFile() {
-  const url = document.getElementById('url').value;
-  const re = /^https?:\/\/emanual\.robotis\.com\/docs\/en\/dxl\/[a-z0-9]+\/[a-z0-9]+-?[a-z0-9]+/
-  const res = url.match(re)
-  if (res !== null && res !== undefined) {
-    const link = res[0];
-    console.log(`Extracted URL as ${link}`);
-    rp(link)
+  
+  function getFile() {
+    const url = document.getElementById('url').value;
+    const re = /^https?:\/\/emanual\.robotis\.com\/docs\/en\/dxl\/[a-z0-9]+\/[a-z0-9]+-?[a-z0-9]+/
+    const res = url.match(re)
+    if (res !== null && res !== undefined) {
+      link = res[0];
+      console.log(`Extracted URL as ${link}`);
+      rp(link)
       .then(function(html) {
         console.log('Successfully downloaded the site!' +
         ' Now scraping the data...');
-        const fileData = generateCSV(html, [1, 2]);
+        fileData = generateCSV(html, [1, 2]);
+        console.log('Downloaded table!');
 
         const linkPieces = link.split('/');
         if (linkPieces[linkPieces.length - 1] == '') {
           linkPieces.pop(linkPieces.length - 1);
         }
+        filename = linkPieces[linkPieces.length - 1];
 
-        const filename = linkPieces[linkPieces.length - 1];
-
-        client.on('error', function(err) {
-          console.error('Unable to connect to the fileserver!' + '\n' + 
-          'Ensure that the robot is powered and active.');
-        })
-        client.connect(5004, '127.0.0.1', function() {
-          client.write(`${filename}\n${fileData}`);
-          client.destroy();
-        })
-
-        console.log('Done!');
+        uploadButton = document.getElementById('upload')
+        uploadButton.innerHTML =
+          `Upload <strong>${filename.replace('-', '')}.csv</strong>`;
+        uploadButton.style.display = null;
       })
       .catch(function(err) {
         console.error('An error has occured!' + '\n' +
@@ -104,8 +106,27 @@ function getFile() {
         'Additionally, check that you are able to access sites' + '\n' +
         'This may be due to you being connected to the robot' + '\n' +
         `Recieved error: \n${err}`);
-    });
+      });
   } else {
     console.error('It seems that your URL is invalid!');
+  } 
+}
+
+function uploadFile() {
+  if (fileData == null || fileData == undefined || fileData == '') {
+    console.error('There is no downloaded file!');
+    return;
   }
+
+  client.connect({
+    port: 5004,
+    address: '127.0.0.1'
+  });
+  
+  client.on('connect', function() {
+    client.write(`${filename}\n${fileData}`);
+    console.log('Wrote file to server!');
+  })
+  
+  console.log('Uploaded table!');
 }
