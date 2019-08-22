9b1541081da4d9fbd4460ba0641a48f0b0a876f3
finchiedev <developer.finchie@gmail.com>
2019-08-22 21:53:23 +0800

    Create wizard files

--- App/JS/main.js
+++ App/JS/main.js
@@ -1,9 +1,13 @@
 const dgram = require('dgram');
 const socket = new dgram.createSocket('udp4');
 
+// Note: this should be made part of the settings later
+const defaultPort = 5001;
+const defaultIP = '127.0.0.1';
+
 socket.on('error', function(err) {
   console.error('UDP client error:\n' + err);
-})
+});
 
 /**
  * Sends information over UDP to the specified IP and port
@@ -11,7 +15,7 @@ socket.on('error', function(err) {
  * @param {*} port The port number of the server
  * @param {*} ip The IP address of the server
  */
-function sendData(data, port, ip) {
+function sendData(data, port = defaultPort, ip = defaultIP) {
   if (data !== null && data !== undefined) {
     if (typeof(data) == 'string' && typeof(port) == 'number' &&
     typeof(ip) == 'string') {
