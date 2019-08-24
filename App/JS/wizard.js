a0846218817803c42bce3e79d541879ce5429e92
finchiedev <developer.finchie@gmail.com>
2019-08-24 13:24:27 +0800

    Implement servo control table viewer

--- App/JS/wizard.js
+++ App/JS/wizard.js
@@ -1,1 +1,31 @@
 const { sendData, Dynamixel } = require('./JS/main.js');
+
+function createRow(items, colTag) {
+  let parent = document.createElement('tr');
+
+  let item;
+  for (let col = 0; col < items.length; col++) {
+    item = document.createElement(colTag);
+    item.style.border = '1px solid black';
+    item.innerHTML = items[col];
+    parent.appendChild(item);
+  }
+
+  return parent;
+}
+
+function createTable(servo) {
+  const csv = servo.controlTableFile;
+
+  let table = document.getElementById('control_table');
+  table.style.border = '1px solid black';
+  table.style.borderCollapse = 'collapse';
+  let header = document.createElement('tr');
+  let headings = csv[0].split(', ');
+
+  table.appendChild(createRow(headings, 'th'));
+
+  for (let line = 1; line < csv.length; line++) {
+    table.appendChild(createRow(csv[line].split(', '), 'td'));
+  }
+}
