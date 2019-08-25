713398dbed0e3f54a21ad45fe65493f15a56274a
finchiedev <developer.finchie@gmail.com>
2019-08-25 15:12:15 +0800

    Implement servo read functionality

--- App/JS/wizard.js
+++ App/JS/wizard.js
@@ -1,4 +1,16 @@
 const { sendData, Dynamixel } = require('./JS/main.js');
+const dgram = require('dgram');
+const server = dgram.createSocket('udp4');
+
+server.on('error', (err) => {
+  console.log(`Server error:\n${err.stack}`);
+  server.close();
+});
+
+server.on('listening', () => {
+  const address = server.address();
+  console.log(`server listening ${address.address}:${address.port}`);
+});
 
 function createModifier(name, id, data) {
   if (data['Access'].includes('W')) {
@@ -39,20 +51,27 @@ function createRow(items, colTag) {
 
 function createTable(servo) {
   const csv = servo.controlTableFile;
-
+  
   let table = document.getElementById('control_table');
   table.style.border = '1px solid black';
   table.style.borderCollapse = 'collapse';
-  let header = document.createElement('tr');
   let headings = csv[0].split(', ');
+  headings.push('Current Value');
+  headings.push('Modify');
 
   table.appendChild(createRow(headings, 'th'));
 
   for (let line = 1; line < csv.length; line++) {
     let row = createRow(csv[line].split(', '), 'td');
-
+    
     const name = csv[line].split(', ')[2]
     const data = servo.controlTable[name];
+
+    const value = document.createElement('td');
+    value.innerHTML = data['InitialValue'];
+    value.id = `${name} Value`;
+    row.appendChild(value);
+
     const modifier = createModifier(name, servo.id, data);
     if (modifier !== null) {
       row.appendChild(modifier);
@@ -60,4 +79,28 @@ function createTable(servo) {
 
     table.appendChild(row);
   }
+}
+
+function initialiseTable(servo) {
+  sendData(`(init)-${servo.model}-${servo.id}-${servo.protocol}`)
+  createTable(servo);
+
+  server.on('message', (msg, rinfo) => {
+    const data = msg.toString().split(':');
+    const name = data[0];
+    const value = data[1];
+
+    // console.log(name, value)
+    const element = document.getElementById(`${name} Value`);
+    if (element == null || element == undefined) {
+      console.log(name)
+    }
+    element.innerHTML = value;
+  });
+
+  server.bind(5003);
+
+  // setInterval(function() {
+  //   sendData(`(read)-${servo.id}-Present Position`);
+  // }, 50)
 }
