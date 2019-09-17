c29ffc873298bbbc87d552244c3ea6867af129d0
finchiedev <developer.finchie@gmail.com>
2019-09-17 21:55:32 +0800

    Add basic page switcher in menu

--- main.js
+++ main.js
@@ -1,25 +1,125 @@
 const {
   app,
-  BrowserWindow
+  BrowserWindow,
+  Menu,
 } = require('electron');
 let win;
 
+/**
+ * Creates a new browser window using the Electron API
+ */
 function createWindow() {
   win = new BrowserWindow({
     width: 1600,
-    height: 1200
+    height: 1200,
   });
-  // win.loadFile('App/loading.html');
-  // setTimeout(function() {
   win.loadFile('App/wizard.html');
-  // }, 3000)
-  // win.loadURL('http://localhost:8080')
   win.webContents.openDevTools();
 
   win.on('closed', () => {
     win = null;
   });
 }
+
+const template = [
+  {
+    label: 'Edit',
+    submenu: [
+      {
+        role: 'undo',
+      },
+      {
+        role: 'redo',
+      },
+      {
+        type: 'separator',
+      },
+      {
+        role: 'cut',
+      },
+      {
+        role: 'copy',
+      },
+      {
+        role: 'paste',
+      },
+    ],
+  },
+  {
+    label: 'View',
+    submenu: [
+      {
+        role: 'reload',
+      },
+      {
+        role: 'toggledevtools',
+      },
+      {
+        type: 'separator',
+      },
+      {
+        role: 'resetzoom',
+      },
+      {
+        role: 'zoomin',
+      },
+      {
+        role: 'zoomout',
+      },
+      {
+        type: 'separator',
+      },
+      {
+        role: 'togglefullscreen',
+      },
+    ],
+  },
+  {
+    role: 'window',
+    submenu: [
+      {
+        role: 'minimize',
+      },
+      {
+        role: 'close',
+      },
+    ],
+  },
+  {
+    label: 'Tools',
+    submenu: [
+      {
+        label: 'Main Driving View',
+        click() {
+          win.loadFile('App/index.html');
+        },
+      },
+      {
+        label: 'Remote Dynamixel Wizard',
+        click() {
+          win.loadFile('App/wizard.html');
+        },
+      },
+      {
+        label: 'Create New Servo',
+        click() {
+          win.loadFile('App/createServo.html');
+        },
+      },
+    ],
+  },
+  {
+    role: 'help',
+    submenu: [
+      {
+        label: 'Learn More',
+      },
+    ],
+  },
+];
+
+const menu = Menu.buildFromTemplate(template);
+Menu.setApplicationMenu(menu);
 
 app.on('ready', createWindow);
 
