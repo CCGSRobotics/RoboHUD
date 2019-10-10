91f06954fd9da6a1bd7c62ca1a96e900b59e58ee
Angus Finch <developer.finchie@gmail.com>
2019-10-10 21:34:45 +0800

    Update style of charts.js

--- App/JS/charts.js
+++ App/JS/charts.js
@@ -1,72 +1,87 @@
-require("charts.js") //https://www.chartjs.org/
+require('charts.js');
 
-function addChart(canvas_id) {
-    // returns the line Chart object
+/**
+ * Creates a chart on the given canvas
+ * @param {String} [id = null] The ID of the canvas
+ * @return {Object} The created line chart
+ */
+function addChart(id = null) {
+  let ctx;
+  if (id === null) {
+    ctx = document.createElement('canvas');
+    document.body.appendChild(ctx);
+  } else {
+    ctx = document.getElementById(canvas_id).getContext('2d');
+  }
 
-    //you can pass a canvas element through or it will create a new one
-    let ctx;
-    if (canvas_id === undefined) {
-        ctx = document.createElement("canvas");
-        document.body.appendChild(ctx);
-    } else {
-        ctx = document.getElementById(canvas_id).getContext('2d');
-    }
-
-    return new Chart(ctx, {
-        "type": "line",
-        "data": {
-            "labels": [],
-            "datasets": []
-        },
-        "options": {}
-    });
+  return new Chart(ctx, {
+    'type': 'line',
+    'data': {
+      'labels': [],
+      'datasets': [],
+    },
+    'options': {},
+  });
 }
 
+/**
+ * Adds a new dataset to the given chart
+ * @param {Object} chart The chart object
+ * @param {String} title The title of the data
+ * @param {String} colour The colour of the data
+ */
 function addDataset(chart, title, colour) {
-    //adds a dataset to the chart datasets array
-    chart.data.datasets.push({
-        "label": title,
-        "data": [],
-        "fill": false,
-        "borderColor": colour,
-        "lineTension": 0.1
-    })
-    chart.update();
+  chart.data.datasets.push({
+    'label': title,
+    'data': [],
+    'fill': false,
+    'borderColor': colour,
+    'lineTension': 0.1,
+  });
+  chart.update();
 }
 
+/**
+ * Adds a datapoint to the chart
+ * @param {Object} chart The chart object
+ * @param {String} dataset The name of the dataset
+ * @param {Number} y The y value of the co-ordinate
+ * @param {Number} [x = CURRENT_TIME] The x value of the co-ordinate
+ */
 function addData(chart, dataset, y, x) {
-    // adds a datapoint to the chart, if x isnt defined then will just use current time
-    if (x === undefined) {
-        var today = new Date();
-        x = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-    }
+  if (x === undefined) {
+    const today = new Date();
+    x = today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
+  }
 
-    //dont want to add the label twice for 1 datapoint
-    if (chart.data.datasets[dataset].data.length === chart.data.labels.length) {
-        chart.data.labels.push(x);
-    }
+  if (chart.data.datasets[dataset].data.length === chart.data.labels.length) {
+    chart.data.labels.push(x);
+  }
 
-    chart.data.datasets[dataset].data.push(y);
+  chart.data.datasets[dataset].data.push(y);
 
 
-    chart.update();
+  chart.update();
 }
 
+/**
+ * Removes data after a given x value
+ * @param {Object} chart The chart object
+ * @param {*} after The index to remove after
+ * @param {*} animation Whether or not to animate the chart
+ */
 function removeDataAfter(chart, after, animation) {
-    // removes old data
-    if (chart.data.labels.length > after) {
+  if (chart.data.labels.length > after) {
+    chart.data.labels.shift();
 
-        chart.data.labels.shift();
+    chart.data.datasets.forEach((dataset) => {
+      dataset.data.shift();
+    });
 
-        chart.data.datasets.forEach((dataset) => {
-            dataset.data.shift();
-        });
-
-        //if the data is being updated less than every second then probs set this to false
-        if (animation) {
-            chart.update();
-        } else {
-            chart.update(0);
-        }
+    if (animation) {
+      chart.update();
+    } else {
+      chart.update(0);
     }
+  }
 }
