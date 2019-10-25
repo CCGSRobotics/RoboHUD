3c71fb79e83bee3e881701a4be17197da66f0393
Angus Finch <developer.finchie@gmail.com>
2019-10-25 22:31:23 +0800

    Add live controller visualisation

--- src/create/controller/controller.js
+++ src/create/controller/controller.js
@@ -2,14 +2,55 @@ const {Controller} = require('../../lib/controller');
 const controllers = [];
 
 /**
+ * Updates the relevant progress bar when called via the 'change' event
+ * @param {Number} index The index of the controller
+ * @param {String} item The name of the node
+ * @param {Number} value The new value of the node
+ */
+function handleChange(index, item, value) {
+  if (document.getElementById(`controller-${index}`) == null) {
+    generateNodes(index);
+  }
+
+  const node = controllers[index].nodes[item];
+  const progress = document.getElementById(`${index}-${item}`);
+  progress.value = node.isButton? value : value + 1;
+}
+
+/**
+ * Generates a progress bar for every node in the controller
+ * @param {Number} index The index of the controller
+ */
+function generateNodes(index) {
+  const controller = controllers[index];
+  const parent = document.createElement('div');
+  parent.id = `controller-${index}`;
+  document.getElementById('controllers').appendChild(parent);
+
+  for (const key in controller.nodes) {
+    if (controller.nodes.hasOwnProperty(key)) {
+      const node = controller.nodes[key];
+      const progress = document.createElement('progress');
+      progress.id = `${controller.index}-${key}`;
+
+      progress.value = node.min;
+      progress.max = node.isButton? node.max : 2;
+      parent.appendChild(progress);
+      parent.appendChild(document.createElement('br'));
+
+      handleChange(index, key, node.value);
+    }
+  }
+}
+
+/**
  * Updates the listings of controllers
  */
 function updateControllers() {
-  const parent = document.getElementById('parent');
   for (let index = 0; index < 4; ++index) {
     const nodes = controllers[index].nodes;
-    controllers[index].onchange = function(item, value) {
-      console.log(`${item}: ${value}`);
+    controllers[index].onchange = (item, value) => {
+      handleChange(index, item, value);
     };
     for (const node in nodes) {
       if (nodes.hasOwnProperty(node)) {
@@ -19,21 +60,43 @@ function updateControllers() {
   }
 }
 
+/**
+ * Handles the connection of a controller
+ * @param {Number} index The index of the controller
+ */
+function handleConnect(index) {
+  console.log('Controller connected');
+  const gamepad = navigator.getGamepads()[index];
+  controllers[index].id = gamepad.id;
+  for (let axis = 0; axis < gamepad.axes.length; ++axis) {
+    controllers[index].addControllerNode(`Unnamed axis #${axis}`,
+        false, axis, [0, 1]);
+  }
+  for (let button = 0; button < gamepad.buttons.length; ++button) {
+    controllers[index].addControllerNode(`Unnamed button #${button}`,
+        true, button, [0, 1]);
+  }
+
+  controllers[index].updateNodes();
+  updateControllers();
+}
+
+/**
+ * Handles the disconnection of a controller
+ * @param {Number} index The index of the controller
+ */
+function handleDisconnect(index) {
+  console.log('Controller disconnected');
+  document.getElementById(`controller-${index}`).remove();
+}
+
 for (let i = 0; i < 4; ++i) {
   controllers.push(new Controller('', i));
   controllers[i].on('connect', () => {
-    console.log('Controller connected');
-    const gamepad = navigator.getGamepads()[i];
-    controllers[i].id = gamepad.id;
-    for (let axis = 0; axis < gamepad.axes.length; ++axis) {
-      controllers[i].addControllerNode(`Unnamed axis #${axis}`, false, axis, [0, 1]);
-    }
-    for (let button = 0; button < gamepad.buttons.length; ++button) {
-      controllers[i].addControllerNode(`Unnamed button #${button}`, true, button, [0, 1]);
-    }
-
-    controllers[i].updateNodes();
-    updateControllers();
+    return handleConnect(i);
+  });
+  controllers[i].on('disconnect', () => {
+    return handleDisconnect(i);
   });
 }
 
@@ -46,12 +109,13 @@ for (let i = 0; i < 4; ++i) {
 function renameNode(oldName, newName, index) {
   const nodes = controllers[index].nodes;
   Object.defineProperty(nodes, newName,
-    Object.getOwnPropertyDescriptor(nodes, oldName));
+      Object.getOwnPropertyDescriptor(nodes, oldName));
   delete nodes[oldName];
 
   controllers[index].updateNodes();
 }
 
+// This should be migrated to animations for cleaner code
 setInterval(() => {
   for (let i = 0; i < 4; ++i) {
     controllers[i].loop();
